c113fcd21d67e62b7e971d376c39db0a601ea0ab
Medeiros <hsymetry@gmail.com>
2022-09-16 23:11:55 -0300

    Add new hero Huntress

--- lib/screens/map_render.dart
+++ lib/screens/map_render.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ng_bonfire/utils/basic_value.dart';
 import 'package:ng_bonfire/widgets/enemies/boss/boss.dart';
 import 'package:ng_bonfire/widgets/enemies/canine/canine.dart';
 import 'package:ng_bonfire/widgets/enemies/firer/firer.dart';
-import 'package:ng_bonfire/widgets/player/player.dart';
+import 'package:ng_bonfire/widgets/player/huntress/huntress.dart';
+import 'package:ng_bonfire/widgets/player/warrior/warrior.dart';
 
 const double tileSize = BasicValues.TILE_SIZE;
 
@@ -21,23 +23,61 @@ class _MapRenderState extends State<MapRender> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     double sizeScreen = min(size.height, size.width) * 2.5;
+
+    var joystick = Joystick(
+      directional: JoystickDirectional(
+        spriteBackgroundDirectional:
+            Sprite.load('decoration/joystick_background.png'),
+        spriteKnobDirectional: Sprite.load('decoration/joystick_knob.png'),
+        size: 100,
+      ),
+      actions: [
+        JoystickAction(
+          actionId: 0,
+          sprite: Sprite.load('decoration/joystick_atack.png'),
+          spritePressed: Sprite.load('decoration/joystick_atack_selected.png'),
+          size: 80,
+          margin: const EdgeInsets.only(bottom: 50, right: 50),
+        ),
+        JoystickAction(
+          actionId: 1,
+          sprite: Sprite.load('decoration/joystick_atack_range.png'),
+          spritePressed:
+              Sprite.load('decoration/joystick_atack_range_selected.png'),
+          size: 50,
+          margin: const EdgeInsets.only(bottom: 50, right: 160),
+        )
+      ],
+    );
+
+    joystick = Joystick(
+      keyboardConfig: KeyboardConfig(
+        keyboardDirectionalType: KeyboardDirectionalType.wasdAndArrows,
+        acceptedKeys: [
+          LogicalKeyboardKey.space,
+          LogicalKeyboardKey.keyZ,
+        ],
+      ),
+    );
+
     return Center(
       child: SizedBox(
         width: sizeScreen,
         height: sizeScreen,
         child: BonfireWidget(
-          showCollisionArea: false,
+          showCollisionArea: true,
           map: WorldMapByTiled(
             'map/gamemap.json',
             forceTileSize: Vector2.all(tileSize),
             objectsBuilder: {
-              'boss': (properties) => Boss(position: Vector2(tileSize * 65, tileSize * 9), ),
-              'firer': (properties) => Firer(position: Vector2(tileSize * 54, tileSize * 17) ),
+              'boss': (properties) => Boss(
+                    position: properties.position,
+                  ),
+              'firer': (properties) => Firer(position: properties.position),
               'canine': (properties) => Canine(position: properties.position),
-              
             },
           ),
-          player: GamePlayer(position: Vector2(tileSize * 20, tileSize * 31)),
+          player: Huntress(position: Vector2(tileSize * 20, tileSize * 31)),
           //Camera
           cameraConfig: CameraConfig(
             moveOnlyMapArea: false,
@@ -45,28 +85,7 @@ class _MapRenderState extends State<MapRender> {
             sizeMovementWindow: Vector2(15, 15),
           ),
           //Joystick
-          joystick: Joystick(
-            keyboardConfig: KeyboardConfig(
-                enable: true,
-                keyboardDirectionalType: KeyboardDirectionalType.wasdAndArrows),
-            directional: JoystickDirectional(
-              spriteBackgroundDirectional:
-                  Sprite.load('decoration/joystick_background.png'),
-              spriteKnobDirectional:
-                  Sprite.load('decoration/joystick_knob.png'),
-              size: 100,
-            ),
-            actions: [
-              JoystickAction(
-                actionId: 0,
-                sprite: Sprite.load('decoration/joystick_atack.png'),
-                spritePressed:
-                    Sprite.load('decoration/joystick_atack_selected.png'),
-                size: 80,
-                margin: const EdgeInsets.only(bottom: 50, right: 80),
-              ),
-            ],
-          ),
+          joystick: joystick,
         ),
       ),
     );
